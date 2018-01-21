ff16871d13b1adf91a055eb6a9357e24512495d3
kwinin <chenchongjian95@163.com>
2018-01-21 18:35:22 +0800

    小远提交

--- models/Users.js
+++ models/Users.js
@@ -3,3 +3,5 @@ var mongoose = require('mongoose');
 var usersSchema = require('../schemas/users');
 
 module.exports=mongoose.model('User',usersSchema);
+
+var usersSchema = require('../schemas/users');
